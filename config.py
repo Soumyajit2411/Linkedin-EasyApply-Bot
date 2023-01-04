ed148ae8404e969b8eb02c74114f944c912dc4ef
Soumyajit Roy <63875591+Soumyajit2411@users.noreply.github.com>
2023-01-05 04:05:24 +0530

    s

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 from dotenv import dotenv_values
+import server
 
 config = dotenv_values(".env")
 browser = ["chrome"]
-email = config['USERNAME']
-password = config['PASSWORD']
+# email = config['USERNAME']
+# password = config['PASSWORD']
+# email = server.result()
+# password = server.result()
 chromeProfilePath = r""
 
 location = []
@@ -24,3 +27,6 @@ workAuthorization = "Yes"
 defaultInput = "1"
 defaultCheck = "Yes"
 defaultRadio = "Yes"
+
+email = '1905641@kiit.ac.in'
+password = 'hggjgjhg'
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, session, url_for
+from typing import List
 
 app = Flask(__name__)
+username = ''
+password = ''
 
 
 @app.route('/')
@@ -11,6 +14,29 @@ def index():
     except:
         return render_template('failure.html')
     return render_template('index.html')
+
+
+@app.route('/input')
+def student():
+    return render_template('input.html')
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result() -> List[str]:
+    if request.method == 'POST':
+        result = request.form
+        username = result["Email"]
+        password = result["Password"]
+        with open("config.py", encoding="utf-8") as file:
+            lines = []
+            for line in file:
+                lines.append(line)
+        url = "email = '" + username + "'\n" + "password = '" + password + "'"
+        with open("config.py", 'w', encoding="utf-8") as f:
+            for line in lines:
+                f.write(line)
+            f.write("\n" + url + "\n")
+    return redirect('/my-link/')
 
 
 @app.route('/my-link/')
@@ -24,4 +50,4 @@ def my_link():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
