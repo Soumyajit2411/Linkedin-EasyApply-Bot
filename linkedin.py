e7e03326e6133ad21f4a9bd5a529c10bc50135b2
Soumyajit Roy <63875591+Soumyajit2411@users.noreply.github.com>
2023-03-13 00:53:10 +0530

    Update linkedin.py

--- linkedin.py
+++ linkedin.py
@@ -45,72 +45,73 @@ class Linkedin:
             try:
                 totalJobss = WebDriverWait(self.driver, delay).until(
                     EC.presence_of_element_located((By.XPATH, '//small')))
-            except TimeoutException:
-                print("Loading took too much time!")
-                self.driver.close()
-            totalJobs = totalJobss.text
-            totalPages = int(utils.jobsToPages(totalJobs))
-            urlWords = utils.urlToKeywords(url)
-            lineToWrite = "\n Category: " + urlWords[
-                0] + ", Location: " + urlWords[1] + ", Applying " + str(
-                    totalJobs) + " jobs."
-            write_results.LinkedinWriteResults().displayWriteResults(
-                lineToWrite)
+                totalJobs = totalJobss.text
+                totalPages = int(utils.jobsToPages(totalJobs))
+                urlWords = utils.urlToKeywords(url)
+                lineToWrite = "\n Category: " + urlWords[
+                    0] + ", Location: " + urlWords[1] + ", Applying " + str(
+                        totalJobs) + " jobs."
+                write_results.LinkedinWriteResults().displayWriteResults(
+                    lineToWrite)
 
-            for page in range(totalPages):
-                currentPageJobs = constants.jobsPerPage * page
-                offersPerPage = alert.LinkedinAlert().jobs_alert(
-                    self.driver, currentPageJobs, url)
-                offerIds = []
-                for offer in offersPerPage:
-                    offerId = offer.get_attribute("data-occludable-job-id")
-                    offerIds.append(int(offerId.split(":")[-1]))
-                for jobID in offerIds:
-                    offerPage = constants.view + str(jobID)
-                    self.driver.get(offerPage)
-                    job_url.LinkedinJobUrlGenerate().storeJobResults(offerPage)
-                    time.sleep(5)
-                    countJobs = str(int(countJobs) + 1)
-                    jobProperties = job_details.LinkedinJobDetails.getJobProperties(
-                        self, self.driver, countJobs)
-                    if "blacklisted" in jobProperties:
-                        lineToWrite = jobProperties + " | " + "Blacklisted Job, skipped!: " + str(
+                for page in range(totalPages):
+                    currentPageJobs = constants.jobsPerPage * page
+                    offersPerPage = alert.LinkedinAlert().jobs_alert(
+                        self.driver, currentPageJobs, url)
+                    offerIds = []
+                    for offer in offersPerPage:
+                        offerId = offer.get_attribute("data-occludable-job-id")
+                        offerIds.append(int(offerId.split(":")[-1]))
+                    for jobID in offerIds:
+                        offerPage = constants.view + str(jobID)
+                        self.driver.get(offerPage)
+                        job_url.LinkedinJobUrlGenerate().storeJobResults(
                             offerPage)
-                        write_results.LinkedinWriteResults(
-                        ).displayWriteResults(lineToWrite)
-                    else:
-                        button = easy_apply_buttons.LinkedinButtons(
-                        ).easyApplyButton(self.driver)
-                        if button is not False:
-                            button.click()
-                            time.sleep(0.5)
-                            countApplied += 1
-                            try:
-                                easy_apply_buttons.LinkedinButtons(
-                                ).chooseButtonConfig(self.driver)
-                                easy_apply_buttons.LinkedinButtons(
-                                ).submitButtonConfig(self.driver)
-                                lineToWrite = jobProperties + " | " + "Just Applied to this job: " + str(
-                                    offerPage)
-                                write_results.LinkedinWriteResults(
-                                ).displayWriteResults(lineToWrite)
-                            except:
-                                try:
-                                    result = next_process.LinkedinNextButtons(
-                                    ).applyProcess(self.driver, offerPage)
-                                    lineToWrite = jobProperties + " | " + result
-                                    write_results.LinkedinWriteResults(
-                                    ).displayWriteResults(lineToWrite)
-                                except:
-                                    lineToWrite = jobProperties + " | " + "Cannot apply to this Job! " + str(
-                                        offerPage)
-                                    write_results.LinkedinWriteResults(
-                                    ).displayWriteResults(lineToWrite)
-                        else:
-                            lineToWrite = jobProperties + " | " + "Already applied! Job: " + str(
+                        time.sleep(5)
+                        countJobs = str(int(countJobs) + 1)
+                        jobProperties = job_details.LinkedinJobDetails.getJobProperties(
+                            self, self.driver, countJobs)
+                        if "blacklisted" in jobProperties:
+                            lineToWrite = jobProperties + " | " + "Blacklisted Job, skipped!: " + str(
                                 offerPage)
                             write_results.LinkedinWriteResults(
                             ).displayWriteResults(lineToWrite)
+                        else:
+                            button = easy_apply_buttons.LinkedinButtons(
+                            ).easyApplyButton(self.driver)
+                            if button is not False:
+                                button.click()
+                                time.sleep(0.5)
+                                countApplied += 1
+                                try:
+                                    easy_apply_buttons.LinkedinButtons(
+                                    ).chooseButtonConfig(self.driver)
+                                    easy_apply_buttons.LinkedinButtons(
+                                    ).submitButtonConfig(self.driver)
+                                    lineToWrite = jobProperties + " | " + "Just Applied to this job: " + str(
+                                        offerPage)
+                                    write_results.LinkedinWriteResults(
+                                    ).displayWriteResults(lineToWrite)
+                                except:
+                                    try:
+                                        result = next_process.LinkedinNextButtons(
+                                        ).applyProcess(self.driver, offerPage)
+                                        lineToWrite = jobProperties + " | " + result
+                                        write_results.LinkedinWriteResults(
+                                        ).displayWriteResults(lineToWrite)
+                                    except:
+                                        lineToWrite = jobProperties + " | " + "Cannot apply to this Job! " + str(
+                                            offerPage)
+                                        write_results.LinkedinWriteResults(
+                                        ).displayWriteResults(lineToWrite)
+                            else:
+                                lineToWrite = jobProperties + " | " + "Already applied! Job: " + str(
+                                    offerPage)
+                                write_results.LinkedinWriteResults(
+                                ).displayWriteResults(lineToWrite)
+            except TimeoutException:
+                print("No Jobs Present!")
+                pass
         self.driver.close()
         return
 
